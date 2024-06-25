6d9f0e4bcdcdb211a8117b91a3476f9766ddee24
Dmitriy Bukharev <needleinspace@gmail.com>
2024-06-25 23:15:41 +0500

    lualine filename relative path

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -20,6 +20,17 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = true,
+    config = function()
+      require('lualine').setup {
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            },
+          },
+        },
+      }
+    end,
   },
 }
